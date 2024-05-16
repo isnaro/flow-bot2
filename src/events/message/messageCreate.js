fa7d967e3e460d1346450560f800a2a2e5e5fbb2
NB <nasrobegg@gmail.com>
2024-05-16 21:57:34 +0100

    Update messageCreate.js

--- src/events/message/messageCreate.js
+++ src/events/message/messageCreate.js
@@ -15,7 +15,7 @@ module.exports = async (client, message) => {
   if (PREFIX_COMMANDS.ENABLED) {
     // check for bot mentions
     if (message.content.includes(`${client.user.id}`)) {
-      message.channel.safeSend(`> My prefix is \`${settings.prefix}\``);
+      message.channel.safeSend(`> matdasrnich `);
     }
 
     if (message.content && message.content.startsWith(settings.prefix)) {
