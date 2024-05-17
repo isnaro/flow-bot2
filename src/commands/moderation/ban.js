64f46d9c9db89820979c91196a1cc81496031a18
NB <nasrobegg@gmail.com>
2024-05-17 16:11:23 +0100

    Update ban.js

--- src/commands/moderation/ban.js
+++ src/commands/moderation/ban.js
@@ -52,10 +52,9 @@ async function ban(issuer, target, reason) {
   try {
     const response = await banTarget(issuer, target, reason);
     if (typeof response === "boolean") {
-      await target.send(`You were banned from FLOW for: ${reason} 
-      
-      ### in case you believe the ban was unfair
-      ### Appeal your ban here : https://discord.gg/m8F8DwXu`).catch(console.error);
+      await target.send(`## You were banned from FLOW for: ${reason} 
+
+      ### in case you believe the ban was unfair, you can appeal your ban here : https://discord.gg/m8F8DwXu`).catch(console.error);
       return `${target.username} is banned!`;
     }
     if (response === "BOT_PERM") return `I do not have permission to ban ${target.username}`;
