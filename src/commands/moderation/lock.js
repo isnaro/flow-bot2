460b62b2539938db4d64fb5052ae8fbb388f46dd
NB <nasrobegg@gmail.com>
2024-05-18 17:51:59 +0100

    update

--- src/commands/moderation/lock.js
+++ src/commands/moderation/lock.js
@@ -1,22 +1,42 @@
-const { Permissions } = require("discord.js");
+const { ApplicationCommandOptionType } = require("discord.js");
 
 module.exports = {
   name: "lock",
   description: "Locks the channel by revoking send messages permission for @everyone",
   category: "UTILITY",
-  botPermissions: ["MANAGE_CHANNELS"],
-  userPermissions: ["MANAGE_CHANNELS"],
-  async execute(message, args) {
+  botPermissions: ["ManageChannels"],
+  userPermissions: ["ManageChannels"],
+  command: {
+    enabled: true,
+    aliases: ["lockchannel"],
+    usage: "",
+  },
+  slashCommand: {
+    enabled: true,
+    options: [],
+  },
+
+  async messageRun(message, args) {
     const channel = message.channel;
 
     try {
-      await channel.permissionOverwrites.edit(message.guild.roles.everyone, {
-        SEND_MESSAGES: false
-      });
-      await message.channel.send("Channel locked successfully.");
+      await channel.permissionOverwrites.edit(message.guild.roles.everyone, { SEND_MESSAGES: false });
+      await message.safeReply("Channel locked successfully.");
     } catch (error) {
       console.error("Error locking channel:", error);
-      await message.channel.send("Failed to lock the channel. Please check the bot's permissions and try again later.");
+      await message.safeReply("Failed to lock the channel. Please try again later.");
     }
-  }
+  },
+
+  async interactionRun(interaction) {
+    const channel = interaction.channel;
+
+    try {
+      await channel.permissionOverwrites.edit(interaction.guild.roles.everyone, { SEND_MESSAGES: false });
+      await interaction.followUp("Channel locked successfully.");
+    } catch (error) {
+      console.error("Error locking channel:", error);
+      await interaction.followUp("Failed to lock the channel. Please try again later.");
+    }
+  },
 };
