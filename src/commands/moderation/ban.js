dc27c10c6e618aeff721ffc09e1a925dbe83db82
NB <nasrobegg@gmail.com>
2024-05-24 04:25:27 +0100

    Update ban.js

--- src/commands/moderation/ban.js
+++ src/commands/moderation/ban.js
@@ -63,12 +63,17 @@ async function ban(issuer, target, reason, duration) {
   const logChannelId = "1225439125776367697"; // Log channel ID
   const logChannel = issuer.guild.channels.cache.get(logChannelId);
 
+  const currentTime = new Date();
+  const gmtPlusOneOffset = 60 * 60 * 1000; // 1 hour in milliseconds
+  const banTime = new Date(currentTime.getTime() + gmtPlusOneOffset);
+  const banTimeString = banTime.toUTCString().replace("GMT", "GMT+1");
+
   try {
     const banMessage = `### 🔴🔴 You were banned from **${issuer.guild.name}** for __***${reason}***__ ###`;
 
     if (duration) {
-      const unbanDate = new Date(Date.now() + duration);
-      const unbanDateString = unbanDate.toUTCString();
+      const unbanDate = new Date(currentTime.getTime() + duration + gmtPlusOneOffset);
+      const unbanDateString = unbanDate.toUTCString().replace("GMT", "GMT+1");
       const durationMessage = `Duration: __***${ms(duration, { long: true })}***__. Your ban will be lifted on: **${unbanDateString}**`;
 
       const dmMessage = `${banMessage}\n\n${durationMessage}\n\n### In case you believe the ban was unfair, you can appeal your ban here: [FLOW Appeal](https://discord.gg/m8F8DwXu) ###`;
@@ -93,6 +98,7 @@ async function ban(issuer, target, reason, duration) {
               { name: "Moderator", value: `${issuer.user.tag} [${issuer.id}]`, inline: true },
               { name: "Reason", value: reason, inline: true },
               { name: "Duration", value: ms(duration, { long: true }), inline: true },
+              { name: "Ban Time", value: banTimeString, inline: true },
               { name: "Unban Date", value: unbanDateString, inline: true },
             )
             .setTimestamp();
@@ -102,6 +108,22 @@ async function ban(issuer, target, reason, duration) {
         setTimeout(async () => {
           try {
             await issuer.guild.members.unban(target.id, "Ban duration expired");
+
+            // Send unban embed to log channel
+            if (logChannel) {
+              const unbanEmbed = new EmbedBuilder()
+                .setTitle("User Unbanned")
+                .setColor("#00FF00")
+                .setThumbnail(target.displayAvatarURL())
+                .addFields(
+                  { name: "User", value: `${target.tag} [${target.id}]`, inline: true },
+                  { name: "Moderator", value: "System", inline: true },
+                  { name: "Reason", value: "Ban duration expired", inline: true },
+                  { name: "Unban Time", value: new Date(Date.now() + gmtPlusOneOffset).toUTCString().replace("GMT", "GMT+1"), inline: true }
+                )
+                .setTimestamp();
+              await logChannel.send({ embeds: [unbanEmbed] });
+            }
           } catch (error) {
             console.error(`Failed to unban ${target.username} after temporary ban:`, error);
           }
@@ -130,6 +152,7 @@ async function ban(issuer, target, reason, duration) {
               { name: "User", value: `${target.tag} [${target.id}]`, inline: true },
               { name: "Moderator", value: `${issuer.user.tag} [${issuer.id}]`, inline: true },
               { name: "Reason", value: reason, inline: true },
+              { name: "Ban Time", value: banTimeString, inline: true },
             )
             .setTimestamp();
           await logChannel.send({ embeds: [embed] });
