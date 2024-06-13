8b1300bcda234b43c8a265432349b1f50476af40
NB <nasrobegg@gmail.com>
2024-06-13 22:22:15 +0100

    .

--- src/events/interactionCreate.js
+++ src/events/interactionCreate.js
@@ -1,29 +1,82 @@
-// src/events/interactionCreate.js
+const { getSettings } = require("@schemas/Guild");
+const { commandHandler, contextHandler, statsHandler, suggestionHandler, ticketHandler } = require("@src/handlers");
+const { InteractionType } = require("discord.js");
+
+/**
+ * @param {import('@src/structures').BotClient} client
+ * @param {import('discord.js').BaseInteraction} interaction
+ */
 module.exports = {
-    name: 'interactionCreate',
-    async execute(interaction) {
-      if (interaction.isButton()) {
-        const commandName = interaction.customId.split('_')[0];
-        const command = interaction.client.slashCommands.get(commandName);
-        if (!command) return;
-  
-        try {
-          await command.execute(interaction);
-        } catch (error) {
-          console.error(error);
-          await interaction.reply({ content: 'There was an error while executing this button interaction!', ephemeral: true });
-        }
-      } else if (interaction.isCommand()) {
-        const command = interaction.client.slashCommands.get(interaction.commandName);
-        if (!command) return;
-  
-        try {
-          await command.execute(interaction);
-        } catch (error) {
-          console.error(error);
-          await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
-        }
+  name: 'interactionCreate',
+  async execute(interaction) {
+    // Ensure interaction occurs in a guild
+    if (!interaction.guild) {
+      return interaction
+        .reply({ content: "Command can only be executed in a discord server", ephemeral: true })
+        .catch(() => {});
+    }
+
+    // Slash Commands
+    if (interaction.isChatInputCommand()) {
+      await commandHandler.handleSlashCommand(interaction);
+    }
+
+    // Context Menu
+    else if (interaction.isContextMenuCommand()) {
+      const context = interaction.client.contextMenus.get(interaction.commandName);
+      if (context) await contextHandler.handleContext(interaction, context);
+      else return interaction.reply({ content: "An error has occurred", ephemeral: true }).catch(() => {});
+    }
+
+    // Buttons
+    else if (interaction.isButton()) {
+      switch (interaction.customId) {
+        case "TICKET_CREATE":
+          return ticketHandler.handleTicketOpen(interaction);
+
+        case "TICKET_CLOSE":
+          return ticketHandler.handleTicketClose(interaction);
+
+        case "SUGGEST_APPROVE":
+          return suggestionHandler.handleApproveBtn(interaction);
+
+        case "SUGGEST_REJECT":
+          return suggestionHandler.handleRejectBtn(interaction);
+
+        case "SUGGEST_DELETE":
+          return suggestionHandler.handleDeleteBtn(interaction);
+
+        default:
+          const commandName = interaction.customId.split('_')[0];
+          const command = interaction.client.slashCommands.get(commandName);
+          if (command) {
+            try {
+              await command.execute(interaction);
+            } catch (error) {
+              console.error(error);
+              await interaction.reply({ content: 'There was an error while executing this button interaction!', ephemeral: true });
+            }
+          }
       }
-    },
-  };
-  
+    }
+
+    // Modals
+    else if (interaction.type === InteractionType.ModalSubmit) {
+      switch (interaction.customId) {
+        case "SUGGEST_APPROVE_MODAL":
+          return suggestionHandler.handleApproveModal(interaction);
+
+        case "SUGGEST_REJECT_MODAL":
+          return suggestionHandler.handleRejectModal(interaction);
+
+        case "SUGGEST_DELETE_MODAL":
+          return suggestionHandler.handleDeleteModal(interaction);
+      }
+    }
+
+    const settings = await getSettings(interaction.guild);
+
+    // Track stats
+    if (settings.stats.enabled) statsHandler.trackInteractionStats(interaction).catch(() => {});
+  }
+};
